239f6fd9f82c71a2c02d07c531787f6c6636ac12
Rahul Badam <rahul.badam@juspay.in>
2025-01-15 09:00:50 +0530

    changed host name

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ function App() {
   const [result, setResult] = useState("");
 
   const runRScript = async () => {
-    const response = await fetch("http://localhost:5000/run-r-script", {
+    const response = await fetch("https://jobs-apply-2.onrender.com/run-r-script", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        params: [5, "Hello from React!"], // Pass parameters to the R script
+        params: [5, "Hello from React!"],
       }),
     });
 
